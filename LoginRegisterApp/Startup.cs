2791d6f1b57dce6f8fff2fda7e45874df64a4784
Francis <francisalcantara001@gmail.com>
2021-11-09 11:36:10 -0400

    update startup file

--- LoginRegisterApp/Startup.cs
+++ LoginRegisterApp/Startup.cs
@@ -24,6 +24,7 @@ namespace LoginRegisterApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,7 @@ namespace LoginRegisterApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +53,7 @@ namespace LoginRegisterApp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
