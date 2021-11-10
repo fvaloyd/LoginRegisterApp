e3fb400e716c626c1ef998a6e31ccb80c13de63b
Francis <francisalcantara001@gmail.com>
2021-11-09 22:52:48 -0400

    modify register and login cs, to redirect to the home controller if you are register

--- LoginRegisterApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LoginRegisterApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -76,6 +76,12 @@ namespace LoginRegisterApp.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("/");
+            }
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
