3e6f4f3c6c98ae2a0c734a900638c056e7e43be1
Francis <francisalcantara001@gmail.com>
2021-11-09 22:47:40 -0400

    authorize homeController

--- LoginRegisterApp/Controllers/HomeController.cs
+++ LoginRegisterApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using LoginRegisterApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,8 +10,10 @@ using System.Threading.Tasks;
 
 namespace LoginRegisterApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
